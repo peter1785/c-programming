a10ca2dfef479b7244eae31fe50bf5fd94a4339a
peter1785 <102529372+peter1785@users.noreply.github.com>
2022-04-01 10:47:24 +0300

    Add files via upload

---
+++ c coding sum of five numbers..c
@@ -0,0 +1,20 @@
+//program find the sum of five numbers
+#include<stdio.h>
+;
+int main()
+{
+float a,b,c,d,e,sum;
+printf("enter the first value a/n");
+scanf("%f",&a);
+printf("enter the second value b/n");
+scanf("%f",&b);
+printf("enter the third value c/n");
+scanf("%f",&c);
+printf("enter the fourth value d/n");
+scanf("%f",&d);
+printf("enter the fifth value e/n");
+scanf("%f",&e);
+sum=a+b+c+d+e;
+printf("sum=%f",a+b+c+d+e);
+return 0;
+}
