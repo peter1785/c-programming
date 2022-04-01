929c421dd2c9af984a38a1c42d951193ad528e71
peter1785 <102529372+peter1785@users.noreply.github.com>
2022-04-01 10:59:39 +0300

    Add files via upload

---
+++ C.coding area of a cicle.c
@@ -0,0 +1,11 @@
+//program for finding area of a cicle
+#include<stdio.h>
+int main()
+{
+float r,Area;
+printf("enter the value of the radiu(r) /n");
+scanf("%f",&r);
+Area=3.142*r*r;
+printf("area=%f/n",3.142*r*r);
+return 0;
+}
---
+++ c.coding_ write your name.c
@@ -0,0 +1,8 @@
+//program for writing your name
+#include<stdio.h>
+
+int main()
+{
+    printf("Name:PETER NDUNGU");
+    return 0;
+}
